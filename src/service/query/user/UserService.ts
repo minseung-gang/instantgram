5806205c42c7b968f77157ff59ca787f4ab38803
minseung-gang <strause1@nate.com>
2024-08-26 14:16:20 +0900

    Chore: 필요없는 코드 삭제

--- src/service/query/user/UserService.ts
+++ src/service/query/user/UserService.ts
@@ -1,6 +1,3 @@
-import { DetailUser } from '@/model/user';
-import { headers } from 'next/headers';
-
 export async function getUser() {
   try {
     const response = await fetch(
@@ -16,5 +13,4 @@ export async function getUser() {
   } catch {
     throw new Error('Failed to fetch user');
   }
-  //기본적으로 fetch를 사용할때 쿠키는 보안상의 이유로 서버 측 페치 호출에서 전달되지 않기때문에 headers를 전달해줘야 한다.
 }
