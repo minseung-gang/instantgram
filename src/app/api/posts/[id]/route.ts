04f04921517c8e174d5e99c87410a656daf1295c
minseung-gang <strause1@nate.com>
2024-08-31 17:37:20 +0900

    Feat: 포스트 데이터 가져오는 api route 추가

---
+++ src/app/api/posts/[id]/route.ts
@@ -0,0 +1,23 @@
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+import { authOptions } from '../../auth/[...nextauth]/route';
+import { getPost } from '@/service/sanity/post';
+
+type Context = {
+  params: { id: string };
+};
+
+export async function GET(request: NextRequest, context: Context) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return NextResponse.json(
+      { message: 'Authentication Error' },
+      { status: 401 },
+    );
+  }
+
+  return getPost(context.params.id) //
+    .then((data) => NextResponse.json(data));
+}
