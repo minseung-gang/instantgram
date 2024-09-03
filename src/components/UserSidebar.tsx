5d42ff01d97080f3103c99cc3790835eb91bece9
minseung-gang <strause1@nate.com>
2024-09-03 14:47:24 +0900

    Design: text 관련 css 추가

--- src/components/UserSidebar.tsx
+++ src/components/UserSidebar.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import React from 'react';
 import Avatar from './Avatar';
 import { User } from '@/model/user';
 import Link from 'next/link';
+import { useDetailPost } from '@/service/post/client/usePostService';
 
 type Props = {
   user: User;
@@ -9,6 +12,7 @@ type Props = {
 
 export default function UserSidebar({ user }: Props) {
   const { username, name, image } = user;
+
   return (
     <div className="pl-10 below-xl:hidden">
       <div className="flex items-center gap-x-2 mt-8 px-3">
@@ -17,9 +21,9 @@ export default function UserSidebar({ user }: Props) {
         </Link>
         <div>
           <Link href={`/user/${name}`}>
-            <p className="text-sm font-bold">{username}</p>
+            <p className="text-sm font-semibold leading-4">{username}</p>
           </Link>
-          <p className="text-sm text-gray-500">{name}</p>
+          <p className="text-sm text-gray-500 leading-4">{name}</p>
         </div>
       </div>
     </div>
