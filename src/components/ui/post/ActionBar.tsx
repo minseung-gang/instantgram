f7986d412b5cd8db21c96f5509d1aa739bc89315
minseung-gang <strause1@nate.com>
2024-09-03 14:49:14 +0900

    Feat: 게시물에 들어갈 좋아요 및 북마크 ui 컴포넌트화

--- src/components/ui/post/ActionBar.tsx
+++ src/components/ui/post/ActionBar.tsx
@@ -6,22 +6,37 @@ import SmileIcon from '../icons/SmileIcon';
 type Props = {
   likes: string[];
   username: string;
-  text: string;
+  text?: string;
 };
 
 export default function ActionBar({ likes, username, text }: Props) {
   return (
     <>
-      <div className="flex justify-between my-3 px-2">
+      <div className="flex justify-between my-3 ">
         <HeartIcon />
         <BookmarkIcon />
       </div>
-      <div className="px-2 py-1">
-        <p className="text-sm font-bold mb-2">{`${likes?.length ?? 0} ${likes?.length > 1 ? 'likes' : 'like'}`}</p>
-        <p className="text-sm">
-          <span className="font-semibold text-sm mr-1">{username}</span>
-          {text}
-        </p>
+      <div className="py-1">
+        {likes?.length > 1 ? (
+          <span className="text-sm mb-2 flex">
+            <p className="text-sm font-semibold">{likes[0]}</p>님&nbsp;
+            <p className="text-sm font-semibold">여러명</p>이 좋아합니다.
+          </span>
+        ) : (
+          <span className="text-sm mb-2 flex">
+            가장 먼저&nbsp;
+            <p className="text-sm font-semibold" role="button">
+              좋아요
+            </p>
+            를 눌러보세요
+          </span>
+        )}
+        {text && (
+          <p className="text-sm">
+            <span className="font-semibold text-sm mr-1">{username}</span>
+            {text}
+          </p>
+        )}
       </div>
     </>
   );
