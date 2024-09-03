916f08e77ad71f101f3614e2ff540ce4dfb98d23
minseung-gang <strause1@nate.com>
2024-09-03 14:53:06 +0900

    Feat: Modal container component 추가

---
+++ src/components/ui/post/PostModal.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import CloseIcon from '../icons/CloseIcon';
+
+type Props = {
+  children: React.ReactNode;
+  onClose: () => void;
+};
+
+export default function PostModal({ onClose, children }: Props) {
+  return (
+    <section
+      className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-[9999] bg-neutral-900/70"
+      onClick={(event) => {
+        if (event.target === event.currentTarget) {
+          onClose();
+        }
+      }}
+    >
+      <button
+        className="fixed right-5 top-5 text-white"
+        onClick={() => onClose()}
+      >
+        <CloseIcon />
+      </button>
+      {children}
+    </section>
+  );
+}
