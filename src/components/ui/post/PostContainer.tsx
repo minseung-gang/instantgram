de0a9bc6dc6a054d2c89a0108f726636e22e20f0
minseung-gang <strause1@nate.com>
2024-09-03 14:50:16 +0900

    Add: dehydrate한 데이터 가져올 api 함수 추가

--- src/components/ui/post/PostContainer.tsx
+++ src/components/ui/post/PostContainer.tsx
@@ -1,9 +1,27 @@
-import queryOptions from '@/service/query/post/queries';
+import queryOptions from '@/service/post/client/queries';
 import { getDehydratedQuery, Hydrate } from '@/utils/react-query';
 import React from 'react';
 import PostList from './PostList';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { cookies } from 'next/headers';
+
+async function getPosts() {
+  try {
+    const response = await fetch(`http://localhost:3000/api/posts`, {
+      method: 'GET',
+      headers: {
+        Cookie: cookies().toString(),
+      },
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch {
+    throw new Error('Failed to fetch post');
+  }
+}
 
 export default async function PostContainer() {
   const session = await getServerSession(authOptions);
@@ -11,7 +29,10 @@ export default async function PostContainer() {
   if (!session) {
     return <div>Authentication required</div>;
   }
-  const { queryFn, queryKey } = queryOptions.all();
+  const { queryFn, queryKey } = {
+    queryKey: ['posts'],
+    queryFn: () => getPosts(),
+  };
   const query = await getDehydratedQuery({ queryKey, queryFn });
 
   return (
