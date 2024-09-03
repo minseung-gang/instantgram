cfa7a72ab5868e723e1e77180529efeb16bb66c3
minseung-gang <strause1@nate.com>
2024-09-03 14:52:40 +0900

    Refactor: ui 컴포넌트화 및 Portal 모달 추가

--- src/components/ui/post/PostListCard.tsx
+++ src/components/ui/post/PostListCard.tsx
@@ -1,41 +1,65 @@
-import Avatar from '@/components/Avatar';
+'use client';
+
 import { SimplePost } from '@/model/post';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { parseDate } from '@/utils/date';
-import CommentForm from '../CommentForm';
 import ActionBar from './ActionBar';
+import SmileIcon from '../icons/SmileIcon';
+import ModalPortal from '../ModalPortal';
+import PostModal from '@/components/ui/post/PostModal';
+import PostDetail from '@/components/ui/post/PostDetail';
+import PostUserAvartar from './PostUserAvartar';
+import useModal from '@/hooks/useModal';
 
 type Props = {
   post: SimplePost;
 };
 export default function PostListCard({ post }: Props) {
-  const { userImage, username, image, createdAt, likes, text } = post;
-  console.log('라잌', likes);
+  const { userImage, username, image, createdAt, likes, text, comments } = post;
+  const { isOpen, openModal, closeModal } = useModal();
+
   return (
     <article>
-      <div className="flex items-center pb-3 pl-1">
-        <Avatar image={userImage} size="small" highlight />
-        <div className="flex gap-x-1 items-center">
-          <span className="text-gray-900 font-semibold text-sm ml-2">
-            {username}
-          </span>
-          <span className="text-sm text-gray-500">•</span>
-          <span className="text-sm text-gray-500 tracking-tight">
-            {parseDate(createdAt)}
-          </span>
-        </div>
-      </div>
+      <PostUserAvartar
+        image={userImage}
+        username={username}
+        createdAt={createdAt}
+      />
       <Image
         src={image}
         alt={`photo by ${username}`}
-        className="w-full object-cover aspect-square"
+        className="w-full object-cover aspect-square pt-3"
         width={500}
         height={500}
         priority
       />
       <ActionBar likes={likes} username={username} text={text} />
-      <CommentForm />
+
+      {comments > 1 ? (
+        <p
+          className="text-sm text-gray-400 mt-1"
+          role="button"
+          onClick={openModal}
+        >
+          댓글 {comments}개 모두 보기
+        </p>
+      ) : null}
+      <form className="flex items-center">
+        <input
+          className="w-full border-none outline-none mt-2 pb-2 text-sm"
+          type="text"
+          placeholder="댓글 달기..."
+        />
+        <SmileIcon size="small" />
+      </form>
+      {isOpen && (
+        <ModalPortal>
+          <PostModal onClose={closeModal}>
+            <PostDetail post={post} />
+          </PostModal>
+        </ModalPortal>
+      )}
     </article>
   );
 }
