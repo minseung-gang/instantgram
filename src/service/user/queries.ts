4bd1d406c11a969cf29a04b300eec25f8301f20b
minseung-gang <strause1@nate.com>
2024-09-04 19:51:41 +0900

    Add: 유저 검색 관련 useQuery문 추가

--- src/service/user/queries.ts
+++ src/service/user/queries.ts
@@ -1,14 +1,19 @@
 import { DetailUser } from '@/model/user';
-import * as UserService from '@/service/query/user/UserService';
+import * as UserService from '@/service/user/UserService';
 
 const queryKeys = {
-  all: ['user'] as const,
+  all: ['users'] as const,
+  search: ['user'] as const,
 };
 
 const queryOptions = {
   all: () => ({
     queryKey: queryKeys.all,
     queryFn: async () => await UserService.getUser(),
+  }),
+  search: (keyword: string) => ({
+    queryKey: queryKeys.search,
+    queryFn: async () => await UserService.serachUser(keyword),
   }),
 };
 
