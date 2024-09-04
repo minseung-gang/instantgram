70b28ade3df226513405b40490cb5efc9f09702d
minseung-gang <strause1@nate.com>
2024-09-04 19:50:58 +0900

    Add 유저 검색 관련 함수 추가

--- src/service/user/UserService.ts
+++ src/service/user/UserService.ts
@@ -14,3 +14,18 @@ export async function getUser() {
     throw new Error('Failed to fetch user');
   }
 }
+
+export async function serachUser(keyword: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/search/${keyword}`,
+      {
+        method: 'GET',
+      },
+    );
+    const data = await response.json();
+    return data;
+  } catch {
+    throw new Error('Failed to search user');
+  }
+}
