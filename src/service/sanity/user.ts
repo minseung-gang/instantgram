1e2927a0e8a772c4ffd4ce289659c2911739e901
minseung-gang <strause1@nate.com>
2024-09-04 19:50:39 +0900

    Add: 유저 검색 관련 함수 추가

--- src/service/sanity/user.ts
+++ src/service/sanity/user.ts
@@ -33,3 +33,17 @@ export async function getUser(username: string) {
       }`,
   );
 }
+
+export async function serachUsers(keyword?: string) {
+  const query = keyword
+    ? `&& (name match "${keyword}") || (username match "${keyword}")`
+    : '';
+  return client.fetch(
+    `*[_type=="user" ${query}]{
+    ...,
+    "following": count(following),
+    "followers": count(followers),
+    }
+    `,
+  );
+}
