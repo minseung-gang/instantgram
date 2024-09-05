32d7958186ac2c56940a4b405f742a7271c2a255
minseung-gang <strause1@nate.com>
2024-09-05 21:42:49 +0900

    Feat: 모달 관련 리듀서 추가

---
+++ src/model/modal.ts
@@ -0,0 +1,18 @@
+export type ModalType = 'search' | 'newPost';
+
+// Define the action types for the modal reducer
+export type ModalAction =
+  | { type: 'OPEN_MODAL'; modalType: ModalType; props?: object }
+  | { type: 'CLOSE_MODAL'; modalType: ModalType }
+  | { type: 'TOGGLE_MODAL'; modalType: ModalType; props?: object };
+
+// Define the state structure for the modals
+export type ModalState = {
+  [key in Exclude<ModalType, null>]: { isOpen: boolean; props: object };
+};
+
+// Initial state of the modal reducer
+export const initialModalState: ModalState = {
+  search: { isOpen: false, props: {} },
+  newPost: { isOpen: false, props: {} },
+};
