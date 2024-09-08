dd30294ebfc8dd6d9ed374824aef3dc03674269d
minseung-gang <strause1@nate.com>
2024-09-08 20:16:44 +0900

    Feat: 유저 검색 관련 스켈레톤 ui 추가

---
+++ src/components/ui/SearchSkeleton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function SearchSkeleton() {
+  return (
+    <div className="flex w-full items-center gap-x-2">
+      <div className="w-10 h-10 bg-gray-100 rounded-full" />
+      <div className="flex flex-col gap-y-2 flex-grow">
+        <div className="w-full h-[16px] rounded-md bg-gray-100" />
+        <div className="w-3/4 h-[16px] rounded-md bg-gray-100" />
+      </div>
+    </div>
+  );
+}
