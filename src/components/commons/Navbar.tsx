310e7162763013c29758ef1abe7f9ac4ff31d3c3
minseung-gang <strause1@nate.com>
2024-09-08 20:12:19 +0900

    Refector: 레이아웃 구조 변경

--- src/components/commons/Navbar.tsx
+++ src/components/commons/Navbar.tsx
@@ -1,50 +1,54 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import NewIcon from '../ui/icons/NewIcon';
 import SearchIcon from '../ui/icons/SearchIcon';
 import HomeIcon from '../ui/icons/HomeIcon';
 import InstaIcon from '../ui/icons/InstaIcon';
 import Link from 'next/link';
-import { signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import LogoutIcon from '../ui/icons/LogoutIcon';
-
-const menu = [
-  {
-    icon: <HomeIcon />,
-    href: '/',
-  },
-  {
-    icon: <SearchIcon />,
-    href: '/search',
-  },
-  {
-    icon: <NewIcon />,
-    href: '/i/flow/upload',
-  },
-];
+import useModal from '@/hooks/useModal';
+import NewPost from '../ui/modal/NewPost';
+import UserSearch from '../ui/modal/UserSearch';
 
 export default function Navbar() {
-  const { data: session } = useSession();
+  const { modalState, openModal, closeModal, toggleModal } = useModal();
+
   return (
-    <header className="sticky top-0 py-8 px-4 h-[100vh] flex flex-col items-center bg-wthie border-r">
+    <header className="sticky top-0 py-8 px-4 h-[100vh] flex flex-col items-center bg-white border-r z-50">
       <InstaIcon />
+      <NewPost
+        isOpen={modalState.newPost.isOpen}
+        onClose={() => toggleModal('newPost')}
+      />
+      <UserSearch
+        isOpen={modalState.search.isOpen}
+        onClose={() => toggleModal('search')}
+      />
       <nav className="flex flex-col h-full justify-between items-center">
         <div className="flex flex-col gap-y-3">
-          {menu.map((item, idx) => {
-            return (
-              <Link
-                key={idx}
-                href={item.href}
-                className="p-3 hover:bg-gray-100 rounded-lg"
-                scroll={false}
-              >
-                {item.icon}
-              </Link>
-            );
-          })}
+          <Link
+            href="/"
+            className="p-3 hover:bg-gray-100 rounded-lg"
+            scroll={false}
+          >
+            <HomeIcon />
+          </Link>
+          <button
+            className="p-3 hover:bg-gray-100 rounded-lg"
+            onClick={() => toggleModal('search')}
+          >
+            <SearchIcon />
+          </button>
+          <button
+            className="p-3 hover:bg-gray-100 rounded-lg"
+            onClick={() => closeModal('newPost')}
+          >
+            <NewIcon />
+          </button>
         </div>
-        {session && <LogoutIcon onClick={() => signOut()} />}
+        <LogoutIcon onClick={() => signOut()} />
       </nav>
     </header>
   );
