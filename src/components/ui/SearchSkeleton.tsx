aa51e50ad3c6a9b1362f80ceb5ac498b8ab5da22
minseung-gang <strause1@nate.com>
2024-09-11 16:43:49 +0900

    Design: padding y값 수정

--- src/components/ui/SearchSkeleton.tsx
+++ src/components/ui/SearchSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function SearchSkeleton() {
   return (
-    <div className="flex w-full items-center gap-x-2">
+    <div className="flex w-full items-center gap-x-2 py-[8px]">
       <div className="w-10 h-10 bg-gray-100 rounded-full" />
       <div className="flex flex-col gap-y-2 flex-grow">
         <div className="w-full h-[16px] rounded-md bg-gray-100" />
