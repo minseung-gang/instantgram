03585cba7388badf253c8e6a569fe8c73c00d4dc
minseung-gang <strause1@nate.com>
2024-09-19 19:22:36 +0900

    Fix: 유저 게시물 관련 컴포넌트 추가 및 ui 구조 변경

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -4,6 +4,7 @@ import UserProfile from '@/components/UserProfile';
 import { notFound } from 'next/navigation';
 import { getDehydratedQuery, Hydrate } from '@/utils/react-query';
 import queryOptions from '@/service/user/server/queries';
+import UserPosts from '@/components/UserPosts';
 
 type Props = {
   params: {
@@ -12,39 +13,17 @@ type Props = {
 };
 
 export default async function Userpage({ params: { username } }: Props) {
+  //유저 프로필 데이터 페칭
   const user = await getUserForProfile(username);
-  const button = [
-    { title: '게시물' },
-    {
-      title: '릴스',
-    },
-    { title: '태그됨' },
-  ];
-  const { queryKey, queryFn } = queryOptions.all();
-  const query = await getDehydratedQuery({ queryKey, queryFn });
+
   if (!user) {
     notFound();
   }
 
   return (
-    <Hydrate state={{ queries: [query] }}>
-      <div className="py-8 px-4">
-        <UserProfile user={user} />
-        <div className="border-t border-gray-200">
-          <div className="flex justify-center gap-x-12">
-            {button.map(({ title }, idx) => (
-              <div key={idx} className="leading-[52px]">
-                <span className="text-xs">{title}</span>
-              </div>
-            ))}
-          </div>
-          <div className="grid grid-flow-col">
-            <div className="w-[300px] aspect-square">d</div>
-            <div className="w-[300px] aspect-square">d</div>
-            <div className="w-[300px] aspect-square">d</div>
-          </div>
-        </div>
-      </div>
-    </Hydrate>
+    <div className="py-8 px-4 max-w-[935px] w-full flex flex-col">
+      <UserProfile user={user} />
+      <UserPosts user={user} />
+    </div>
   );
 }
