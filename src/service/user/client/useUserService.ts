454a0c5acaaa3fa107f610a9445003d9e8fdd49f
minseung-gang <strause1@nate.com>
2024-09-19 19:34:00 +0900

    Feat: 유저 프로필 게시물 관련 useQuery 함수 추가

--- src/service/user/client/useUserService.ts
+++ src/service/user/client/useUserService.ts
@@ -1,6 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import queryOptions from './queries';
+import { SimplePost } from '@/model/post';
 
 export function useSearchUsers(keyword: string) {
   return useQuery(queryOptions.search(keyword));
+}
+export function useUserPost(username: string, tab: string) {
+  return useQuery<SimplePost[]>(queryOptions.post(username, tab));
 }
