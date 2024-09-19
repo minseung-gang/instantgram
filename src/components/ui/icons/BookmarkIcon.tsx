d1c23049f463147a7eeb3d89619ca19410b9a191
minseung-gang <strause1@nate.com>
2024-09-19 19:26:24 +0900

    Add: className을 Props받도록 수정

--- src/components/ui/icons/BookmarkIcon.tsx
+++ src/components/ui/icons/BookmarkIcon.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { RiBookmarkLine } from 'react-icons/ri';
 import { RiBookmarkFill } from 'react-icons/ri';
 
-export default function BookmarkIcon() {
-  return <RiBookmarkLine className="w-6 h-6" />;
+type Props = {
+  className: string;
+};
+
+export default function BookmarkIcon({ className }: Props) {
+  return <RiBookmarkLine className={className || 'w-6 h-6'} />;
 }
