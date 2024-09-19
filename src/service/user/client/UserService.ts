e68617d9078bbb27a9138fbf9e9383f74d170cbf
minseung-gang <strause1@nate.com>
2024-09-19 19:34:33 +0900

    Feat: 유저 프로필 게시물 관련 fetch 함수 추가

--- src/service/user/client/UserService.ts
+++ src/service/user/client/UserService.ts
@@ -14,3 +14,21 @@ export async function serachUser(keyword: string) {
     throw new Error('Failed to search user');
   }
 }
+
+export async function getUserPost(username: string, tab: string) {
+  console.log('username', username, 'tab', tab);
+  try {
+    const response = await fetch(
+      ` ${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/api/users/${username}/${tab}`,
+      {
+        method: 'GET',
+      },
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch {
+    throw new Error('Failed to fetch user');
+  }
+}
