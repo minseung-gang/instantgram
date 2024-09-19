dbce75007d8352c9fb5eff78f753a602f4af0122
minseung-gang <strause1@nate.com>
2024-09-19 19:31:07 +0900

    Feat: 유저 프로필 게시물 관련 Container Component 추가

---
+++ src/components/PostGrid.tsx
@@ -0,0 +1,28 @@
+import { useUserPost } from '@/service/user/client/useUserService';
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
+import PostGridCard from './PostGridCard';
+
+type Props = {
+  username: string;
+  query: string;
+};
+
+export default function PostGrid({ username, query }: Props) {
+  const { data: posts, isLoading, error } = useUserPost(username, query);
+
+  return (
+    <div className="flex justify-center">
+      {isLoading && <ClipLoader />}
+      {posts && (
+        <ul className="w-full grid grid-cols-3 gap-1">
+          {posts.map((post, idx) => (
+            <li key={post.id} className="aspect-square relative">
+              <PostGridCard post={post} priority={idx < 6} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
