c2034165f003f5fc9d509c11d725597f99ff9911
minseung-gang <strause1@nate.com>
2024-09-20 17:34:16 +0900

    Feat: 유저 프로필 데이터 캐싱 및 메타데이터 생성 기능 추가

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,10 +1,11 @@
 import { getUserForProfile } from '@/service/sanity/user';
-import React from 'react';
+import React, { cache } from 'react';
 import UserProfile from '@/components/UserProfile';
 import { notFound } from 'next/navigation';
 import { getDehydratedQuery, Hydrate } from '@/utils/react-query';
 import queryOptions from '@/service/user/server/queries';
 import UserPosts from '@/components/UserPosts';
+import { Metadata } from 'next';
 
 type Props = {
   params: {
@@ -12,9 +13,11 @@ type Props = {
   };
 };
 
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function Userpage({ params: { username } }: Props) {
   //유저 프로필 데이터 페칭
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
 
   if (!user) {
     notFound();
@@ -26,4 +29,14 @@ export default async function Userpage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </div>
   );
+}
+
+export async function generateMetadata({
+  params: { username },
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${user?.username}) · Instantgram Photos`,
+    description: `${user?.name}'s all Instantgram posts`,
+  };
 }
