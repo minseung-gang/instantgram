c2556bc30eb04ebddd805f715291d44a18335593
minseung-gang <strause1@nate.com>
2024-09-23 17:40:37 +0900

    Feat: 실시간 데이터 페칭을 위해 다이나믹 설정 추가

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,8 @@
 import { serachUsers } from '@/service/sanity/user';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   return serachUsers().then((data) => NextResponse.json(data));
 }
