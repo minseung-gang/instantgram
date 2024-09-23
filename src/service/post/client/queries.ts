8d09cb1f5a474c1b5c4c205083ddee258c99177b
minseung-gang <strause1@nate.com>
2024-09-23 18:05:23 +0900

    Feat: 좋아요 관련 queryOptions 기능 추가

--- src/service/post/client/queries.ts
+++ src/service/post/client/queries.ts
@@ -1,4 +1,7 @@
+import { SimplePost } from '@/model/post';
 import * as PostService from '@/service/post/client/PostService';
+import { QueryClient } from '@tanstack/react-query';
+
 export const queryKeys = {
   all: ['posts'] as const,
 
@@ -29,8 +32,25 @@ const queryOptions = {
 
   comment: ({ postId, commentId }: { postId: string; commentId: number }) => ({
     queryKey: queryKeys.detailComment({ postId, commentId }),
-
     queryFn: () => PostService.getComment({ postId, commentId }),
+  }),
+
+  like: (queryClient: QueryClient, postId: string, currentUser: string) => ({
+    mutationFn: async (liked: boolean) =>
+      await PostService.updateLike(liked, postId),
+
+    onMutate: async () =>
+      await PostService.OptimisticLikeUpdate(queryClient, postId, currentUser),
+
+    onError: (context: { previousPosts: SimplePost[] | undefined }) => {
+      // 오류 발생 시 원래 상태로 복구
+      queryClient.setQueryData(queryKeys.all, context.previousPosts);
+    },
+
+    onSettled: () => {
+      // 서버 응답 후 최신 데이터 가져오기 (이 부분에서 무효화 후 데이터 fetch)
+      queryClient.invalidateQueries({ queryKey: queryKeys.all });
+    },
   }),
 };
 
