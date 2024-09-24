c98a02caf89133b03fbb1d4e9481716bd39c8dc4
minseung-gang <strause1@nate.com>
2024-09-24 21:01:19 +0900

    Chore: 컴포넌트 import 경로 수정

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from '../../auth/[...nextauth]/route';
+import { authOptions } from '@/app/auth';
 import { getPost } from '@/service/sanity/post';
 
 type Context = {
