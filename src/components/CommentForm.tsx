0364165afc2add13b58720dfa131cb7f05a8eaa6
minseung-gang <strause1@nate.com>
2024-09-27 21:44:20 +0900

    Refector submit 함수 추가 및 ui 수정

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import SmileIcon from '@/components/ui/icons/SmileIcon';
 
-export default function CommentForm() {
+type Props = {
+  onPostComment: (comment: string) => void;
+};
+
+export default function CommentForm({ onPostComment }: Props) {
+  const [comment, setComment] = useState('');
+  const buttonDisabled = comment.length === 0;
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onPostComment(comment);
+    setComment('');
+  };
   return (
-    <form className="flex items-center">
+    <form className="flex items-center py-2 pr-4 border-t border-gray-200">
+      <div className="px-4 py-2">
+        <SmileIcon />
+      </div>
       <input
-        className="w-full border-none outline-none mt-2 pb-2 text-sm"
+        className="w-full outline-none text-sm placeholder:text-gray-500"
         type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
         placeholder="댓글 달기..."
       />
-      <SmileIcon />
+      <button
+        disabled={buttonDisabled}
+        className={`min-w-fit text-sm font-bold ${buttonDisabled ? 'text-blue-300' : 'text-blue-500'}`}
+        onClick={handleSubmit}
+      >
+        게시
+      </button>
     </form>
   );
 }
