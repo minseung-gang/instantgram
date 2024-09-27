eb8130e0c7ca605d47c09294deac49126e64580c
minseung-gang <strause1@nate.com>
2024-09-27 22:50:07 +0900

    chore: 필요없는 코드 삭제

--- src/components/post/PostContainer.tsx
+++ src/components/post/PostContainer.tsx
@@ -1,18 +1,14 @@
 import { getDehydratedQuery, Hydrate } from '@/utils/react-query';
 import React from 'react';
 import PostList from './PostList';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/auth';
-import { cookies } from 'next/headers';
+
 import postQueryOptions from '@/service/post/server/queries';
 import userQueryOptions from '@/service/user/server/queries';
 
 export default async function PostContainer() {
   const postQuery = await getDehydratedQuery(postQueryOptions.all());
   const userQuery = await getDehydratedQuery(userQueryOptions.all());
-  const session = await getServerSession(authOptions);
 
-  const user = session?.user?.username || '';
   return (
     <Hydrate state={{ queries: [postQuery, userQuery] }}>
       <PostList />
