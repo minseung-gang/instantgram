b01133ed1d1eeeb053374890b497d679770df78d
minseung-gang <strause1@nate.com>
2024-09-27 21:46:56 +0900

    FeatL 댓글 관련 mutate함수 추가

--- src/service/post/client/usePostService.ts
+++ src/service/post/client/usePostService.ts
@@ -1,9 +1,4 @@
-import {
-  useMutation,
-  useQuery,
-  QueryClient,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import queryOptions from '@/service/post/client/queries';
 
@@ -11,21 +6,12 @@ export function useDetailPost({ postId }: { postId: string }) {
   return useQuery(queryOptions.detail(postId));
 }
 
-export function useComments({ postId }: { postId: string }) {
-  return useQuery(queryOptions.comments(postId));
-}
-
-export function useComment({
-  postId,
-  commentId,
-}: {
-  postId: string;
-  commentId: number;
-}) {
-  return useQuery(queryOptions.comment({ postId, commentId }));
-}
-
 export function useLikePost(postId: string, currentUser: string) {
   const queryClient = useQueryClient();
   return useMutation(queryOptions.like(queryClient, postId, currentUser));
+}
+
+export function useComments(postId: string) {
+  const queryClient = useQueryClient();
+  return useMutation(queryOptions.comment(queryClient, postId));
 }
