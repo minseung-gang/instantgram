54fde58d58472aef0d4d149b8ccf18378e73dd6b
minseung-gang <strause1@nate.com>
2024-09-30 19:15:06 +0900

    Refector : follow upate 관련 mutate함수 적용

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { HomeUser, ProfileUser } from '@/model/user';
+import { useFollow } from '@/service/user/client/useUserService';
 import { useQuery } from '@tanstack/react-query';
-import { useSession } from 'next-auth/react';
-import React from 'react';
 
 type Props = {
   user: ProfileUser;
@@ -20,7 +19,7 @@ async function fetchUser() {
 }
 
 export default function FollowButton({ user }: Props) {
-  const { username } = user;
+  const { id, username } = user;
   const { data: loggedInUser } = useQuery<HomeUser>({
     queryKey: ['users'],
     queryFn: async () => fetchUser(),
@@ -29,14 +28,20 @@ export default function FollowButton({ user }: Props) {
   const showButton = loggedInUser && loggedInUser?.username !== username;
   const following =
     loggedInUser &&
-    loggedInUser?.following?.find((item) => item.username == username);
+    loggedInUser?.following?.find((item) => item.username === username);
   const text = following ? '팔로잉' : '팔로우';
 
+  const { mutate: toggleFollow } = useFollow(id, user.username);
+
+  const handleFollow = () => {
+    user && toggleFollow(!following);
+  };
   return (
     <>
       {showButton && (
         <button
           className={`px-4 py-[6px] ${following ? 'bg-gray-200' : 'bg-[#0095F6]'} ${following ? 'black' : 'text-white'} rounded-md text-sm font-semibold`}
+          onClick={handleFollow}
         >
           {text}
         </button>
