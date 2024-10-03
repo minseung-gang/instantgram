470cf105321a35cf21db00098b561762e9a4a681
minseung-gang <strause1@nate.com>
2024-10-03 22:22:19 +0900

    Refector: queryClinet를 인자로 전달하도록 수정

--- src/service/post/client/usePostService.ts
+++ src/service/post/client/usePostService.ts
@@ -1,14 +1,27 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
 
 import queryOptions from '@/service/post/client/queries';
+import { cache } from 'react';
 
 export function useDetailPost({ postId }: { postId: string }) {
   return useQuery(queryOptions.detail(postId));
 }
 
-export function useLikePost(postId: string, currentUser: string) {
-  const queryClient = useQueryClient();
-  return useMutation(queryOptions.like(queryClient, postId, currentUser));
+export function useLikePost(
+  queryClient: QueryClient,
+  postId: string,
+  username: string,
+  cacheKey: string | string[],
+) {
+  const queryKey = Array.isArray(cacheKey) ? cacheKey : [cacheKey];
+  return useMutation(
+    queryOptions.like(queryClient, postId, username, queryKey),
+  );
 }
 
 export function useComments(postId: string) {
