5a9914cd042898228ba844467320bdc17fe89867
minseung-gang <strause1@nate.com>
2024-10-03 22:23:21 +0900

    Refector: 필요없는 코드 제거 및 comment,like 관련 query추가

--- src/service/post/client/queries.ts
+++ src/service/post/client/queries.ts
@@ -4,46 +4,23 @@ import { QueryClient } from '@tanstack/react-query';
 
 export const queryKeys = {
   all: ['posts'] as const,
-
   detail: (postId: string) => [postId] as const,
-
-  detailComments: (postId: string) =>
-    [...queryKeys.detail(postId), 'comments'] as const,
-
-  detailComment: ({
-    postId,
-    commentId,
-  }: {
-    postId: string;
-    commentId: number;
-  }) => [...queryKeys.detailComments(postId), commentId] as const,
 };
 
 const queryOptions = {
   detail: (postId: string) => ({
     queryKey: queryKeys.detail(postId),
     queryFn: () => PostService.getDetailPost(postId),
+    staleTime: 0,
+    cacheTime: 0,
   }),
 
-  comments: (postId: string) => ({
-    queryKey: queryKeys.detailComments(postId),
-    queryFn: () => PostService.getComments(postId),
-  }),
-
-  comment: ({ postId, commentId }: { postId: string; commentId: number }) => ({
-    queryKey: queryKeys.detailComment({ postId, commentId }),
-
-    queryFn: () => PostService.getComment({ postId, commentId }),
-  }),
-
-  like: (queryClient: QueryClient, postId: string, currentUser: string) => ({
-    mutationFn: async (liked: boolean) =>
-      await PostService.updateLike(liked, postId),
-
-    onMutate: async (liked: boolean) =>
-      await PostService.OptimisticLike(queryClient, postId, currentUser, liked),
-
-    onError: (context: { previousPosts: SimplePost[] | undefined }) => {
+  comment: (queryClient: QueryClient, postId: string) => ({
+    mutationFn: async (comment: string) =>
+      await PostService.addComment(comment, postId),
+    onMutate: async () =>
+      await PostService.OptimisticComment(queryClient, postId),
+    Error: (context: { previousPosts: SimplePost[] | undefined }) => {
       // 오류 발생 시 원래 상태로 복구
       queryClient.setQueryData(['posts'], context.previousPosts);
     },
@@ -51,6 +28,40 @@ const queryOptions = {
     onSettled: () => {
       // 서버 응답 후 최신 데이터 가져오기 (이 부분에서 무효화 후 데이터 fetch)
       queryClient.invalidateQueries({ queryKey: ['posts'] });
+      queryClient.refetchQueries({ queryKey: [postId] });
+    },
+  }),
+
+  like: (
+    queryClient: QueryClient,
+    postId: string,
+    username: string,
+    queryKey: string[],
+  ) => ({
+    mutationFn: async (liked: boolean) =>
+      await PostService.updateLike(liked, postId),
+
+    onMutate: async (liked: boolean) =>
+      await PostService.OptimisticLike(
+        queryClient,
+        postId,
+        username,
+        liked,
+        queryKey,
+      ),
+
+    onError: (context: { previousPosts: SimplePost[] }) => {
+      // 오류 발생 시 원래 상태로 복구
+      console.log('오류');
+      queryClient.setQueryData(queryKey, context.previousPosts);
+    },
+
+    onSetteld: () => {
+      console.log('키키');
+      // 서버 응답 후 최신 데이터 가져오기 (이 부분에서 무효화 후 데이터 fetch)
+      queryClient.invalidateQueries({
+        queryKey: queryKey,
+      });
     },
   }),
 };
