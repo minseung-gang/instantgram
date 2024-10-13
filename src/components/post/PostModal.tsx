ef4c9aa9ea91b4cecbbb6bb4e5a7e05976309ff6
minseung-gang <strause1@nate.com>
2024-10-13 23:09:09 +0900

    Refector: 마우스 이벤트 관련 props 전달받도록 코드 수정

--- src/components/post/PostModal.tsx
+++ src/components/post/PostModal.tsx
@@ -4,9 +4,20 @@ import CloseIcon from '../ui/icons/CloseIcon';
 type Props = {
   children: React.ReactNode;
   onClose: () => void;
+  onDragEnter?: (e: React.DragEvent) => void;
+  onDragLeave?: (e: React.DragEvent) => void;
+  onDragOver?: (e: React.DragEvent) => void;
+  onDrop?: (e: React.DragEvent) => void;
 };
 
-export default function PostModal({ onClose, children }: Props) {
+export default function PostModal({
+  onClose,
+  onDragEnter,
+  onDragLeave,
+  onDragOver,
+  onDrop,
+  children,
+}: Props) {
   return (
     <section
       className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-[9999] bg-neutral-900/70"
@@ -15,6 +26,10 @@ export default function PostModal({ onClose, children }: Props) {
           onClose();
         }
       }}
+      onDragEnter={onDragEnter}
+      onDragLeave={onDragLeave}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
     >
       <button
         className="fixed right-5 top-5 text-white"
