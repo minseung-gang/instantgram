85e580edc1df61aa4b213e11e8e423993b227974
minseung-gang <strause1@nate.com>
2024-10-13 23:09:43 +0900

    Feat: 파일아이콘 관련 컴포넌트 추가

---
+++ src/components/ui/icons/FilesIcons.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { FaPhotoVideo } from 'react-icons/fa';
+
+export default function FilesIcons() {
+  return <FaPhotoVideo className="w-24 h-24 text-gray-300" />;
+}
