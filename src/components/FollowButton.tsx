9df4621ddae180052f3242fd1e4ef29cb6a74b97
minseung-gang <strause1@nate.com>
2024-10-13 23:06:34 +0900

    Refector: 공통 Button 컴포넌트를 사용하도록 수정

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import revalidateProfileUser from '@/actions/actions';
+import revalidateProfileUser from '@/utils/user';
 import { HomeUser, ProfileUser } from '@/model/user';
 import { useFollow } from '@/service/user/client/useUserService';
 import { useQuery } from '@tanstack/react-query';
 import { startTransition, useState, useTransition } from 'react';
 
 import { ClipLoader } from 'react-spinners';
+import Button from './ui/Button';
 
 type Props = {
   user: ProfileUser;
@@ -35,11 +36,12 @@ export default function FollowButton({ user }: Props) {
     loggedInUser &&
     loggedInUser?.following?.find((item) => item.username === username);
   const text = following ? '팔로잉' : '팔로우';
+  const disabled = text === '팔로잉';
   const [isPending, startTransition] = useTransition();
   const [isFetching, setIsFetching] = useState(false);
   const isUpdating = isPending || isFetching;
   const { mutate: toggleFollow } = useFollow(id, username);
-  const disabled = text === '팔로잉';
+
   const handleFollow = async () => {
     setIsFetching(true);
     toggleFollow(!following, {
@@ -58,23 +60,23 @@ export default function FollowButton({ user }: Props) {
   return (
     <>
       {showButton && (
-        <button
-          className={`relative px-4 py-[6px] ${disabled ? 'bg-gray-200' : 'bg-[#0095F6]'} ${disabled ? 'black' : 'text-white'} rounded-md text-sm font-semibold`}
-          onClick={handleFollow}
-        >
-          <div className={`${isUpdating ? 'invisible' : 'visible'}`}>
-            {text}
-          </div>
-
+        <div className="relative">
+          <Button
+            className={`relative px-4 py-[6px] ${disabled ? 'bg-gray-200' : 'bg-[#0095F6]'} ${disabled ? 'black' : 'text-white'} rounded-md text-sm font-semibold`}
+            onClick={handleFollow}
+            disabled={isUpdating}
+            isLoading={isUpdating}
+            text={text}
+          />
           {isUpdating && (
-            <div className="absolute inset-0 flex justify-center items-center">
+            <div className="absolute z-20 inset-0 flex justify-center items-center">
               <ClipLoader
                 className={`!w-4 !h-4 `}
                 color={disabled ? 'gray' : 'white'}
               />
             </div>
           )}
-        </button>
+        </div>
       )}
     </>
   );
