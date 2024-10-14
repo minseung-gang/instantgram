4ad84668eb2fd211c12ee01f7295d8cf25d3aa6a
minseung-gang <strause1@nate.com>
2024-10-14 18:22:09 +0900

    Feat: 게시물 업로드를 위한 useUploadPost 함수 추가

--- src/service/post/client/usePostService.ts
+++ src/service/post/client/usePostService.ts
@@ -4,9 +4,7 @@ import {
   useQuery,
   useQueryClient,
 } from '@tanstack/react-query';
-
 import queryOptions from '@/service/post/client/queries';
-import { cache } from 'react';
 
 export function useDetailPost({ postId }: { postId: string }) {
   return useQuery(queryOptions.detail(postId));
@@ -27,4 +25,11 @@ export function useLikePost(
 export function useComments(postId: string) {
   const queryClient = useQueryClient();
   return useMutation(queryOptions.comment(queryClient, postId));
+}
+
+export function useUploadPost(
+  queryClient: QueryClient,
+  onSuccessCallback?: () => void,
+) {
+  return useMutation(queryOptions.upload(queryClient, onSuccessCallback));
 }
