ee02752311f6c76b9523fd80567373e00e6ddc16
minseung-gang <strause1@nate.com>
2024-10-14 18:18:04 +0900

    Feat: 텍스트와 파일을 업로드하여 게시물을 생성하는 POST API 추가

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,8 +1,8 @@
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/app/auth';
 import { getUser } from '@/service/sanity/user';
-import { NextResponse } from 'next/server';
-import { getFollowingPostsOf } from '@/service/sanity/post';
+import { NextRequest, NextResponse } from 'next/server';
+import { createPost, getFollowingPostsOf } from '@/service/sanity/post';
 
 export async function GET() {
   const session = await getServerSession(authOptions);
@@ -20,6 +20,36 @@ export async function GET() {
     const postData = await getFollowingPostsOf(user.username);
 
     return NextResponse.json(postData, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Failed to fetch user' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  const user = session?.user;
+
+  if (!user) {
+    return NextResponse.json(
+      { message: 'Authentication Error' },
+      { status: 401 },
+    );
+  }
+
+  try {
+    const form = req.formData();
+    const text = (await form).get('text')?.toString() ?? '';
+    const file = (await form).get('file') as Blob;
+
+    if (!file) {
+      return new Response('Bad Request', { status: 400 });
+    }
+    const response = await createPost(user.id, text, file);
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: 'Failed to fetch user' },
