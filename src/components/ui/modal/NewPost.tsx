900bd3fa26be6ff62fb2bd77c4822acc59af51f7
minseung-gang <strause1@nate.com>
2024-10-14 18:22:55 +0900

    Refector: 필요없는 useState 코드 제거 및 mutate함수 적용

--- src/components/ui/modal/NewPost.tsx
+++ src/components/ui/modal/NewPost.tsx
@@ -13,6 +13,9 @@ import { AuthUser } from '@/model/user';
 import FileUploadForm from '@/components/newpost/FileUploadForm';
 import UserSection from '@/components/UserSection';
 import { useRouter } from 'next/navigation';
+import { useUploadPost } from '@/service/post/client/usePostService';
+import { useQueryClient } from '@tanstack/react-query';
+import { ClipLoader } from 'react-spinners';
 
 type Props = {
   user?: AuthUser;
@@ -24,11 +27,13 @@ export default function NewPost({ user, isOpen, onClose }: Props) {
   const [file, setFile] = useState<File>();
   const [dragging, setDragging] = useState(false);
   const [charCount, setCharCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
   const textRef = useRef<HTMLTextAreaElement>(null);
   const router = useRouter();
-
+  const queryClient = useQueryClient();
+  const { mutate, isPending } = useUploadPost(queryClient, () => {
+    router.push('/');
+    onClose();
+  });
   // 파일 선택 핸들러
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target?.files;
@@ -65,26 +70,13 @@ export default function NewPost({ user, isOpen, onClose }: Props) {
   // 전송 이벤트 핸들러
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    if (!file) return;
-    setLoading(true);
 
+    if (!file) return;
     const formData = new FormData();
     formData.append('file', file);
     formData.append('text', textRef.current?.value ?? '');
 
-    fetch('/api/posts/', {
-      method: 'POST',
-      body: formData,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          setError(`${res.status} ${res.statusText}`);
-          return;
-        }
-        router.push('/');
-      })
-      .catch((error: any) => setError(error.toString()))
-      .finally(() => setLoading(false));
+    mutate(formData);
   };
 
   // 입력 텍스트 업데이트
@@ -114,47 +106,55 @@ export default function NewPost({ user, isOpen, onClose }: Props) {
         <div className={`bg-white rounded-lg`}>
           <div className="flex flex-col">
             <div className="relative font-semibold py-2 text-center border-b border-gray-300">
-              새 게시물 만들기
+              {isPending ? '공유중입니다' : '새 게시물 만들기'}
               <span
-                className="absolute right-4 top-1/2 text-sm transform -translate-y-1/2 cursor-pointer text-[#0095F6] hover:text-black"
+                className="absolute right-4 text-sm top-1/2 transform -translate-y-1/2 cursor-pointer text-[#0095F6] hover:text-black"
                 onClick={handleSubmit}
               >
                 공유하기
               </span>
             </div>
-            {loading && <div>dd</div>}
-            {error && <p>{error}</p>}
+
             <div
-              className="flex flex-grow transition-all duration-500 ease-in-out"
+              className="flex-grow transition-all duration-500 ease-in-out"
               style={file ? { width: '1040px' } : { width: '700px' }}
             >
-              <FileUploadForm
-                dragging={dragging}
-                file={file}
-                change={handleChange}
-              />
+              <div className="relative">
+                <div className="flex">
+                  {isPending && (
+                    <div className="absolute w-full h-full left-0 top-0 flex items-center justify-center bg-white z-50">
+                      <ClipLoader size={90} />
+                    </div>
+                  )}
+                  <FileUploadForm
+                    dragging={dragging}
+                    file={file}
+                    change={handleChange}
+                  />
 
-              {file && (
-                <div className="flex-grow">
-                  <UserSection user={user} />
-                  <div>
-                    <textarea
-                      className="w-full outline-none px-5 resize-none"
-                      name="text"
-                      id="input-text"
-                      required
-                      rows={8}
-                      ref={textRef}
-                      onChange={handleTextChange}
-                    />
-                  </div>
-                  <div className="leading-10 px-4 border-b border-gray-300 text-end">
-                    <span className="text-xs text-gray-300">
-                      {charCount}/1000
-                    </span>
-                  </div>
+                  {file && (
+                    <div className="flex-grow">
+                      <UserSection user={user} />
+                      <div>
+                        <textarea
+                          className="w-full outline-none px-5 resize-none"
+                          name="text"
+                          id="input-text"
+                          required
+                          rows={8}
+                          ref={textRef}
+                          onChange={handleTextChange}
+                        />
+                      </div>
+                      <div className="leading-10 px-4 border-b border-gray-300 text-end">
+                        <span className="text-xs text-gray-300">
+                          {charCount}/1000
+                        </span>
+                      </div>
+                    </div>
+                  )}
                 </div>
-              )}
+              </div>
             </div>
           </div>
         </div>
