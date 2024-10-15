80eb2e5154a7ff88360f451a07d129b9306a2af4
minseung-gang <strause1@nate.com>
2024-10-15 16:57:20 +0900

    Refector:  query dehydration 로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import UserSidebar from '@/components/UserSidebar';
 import Followingbar from '@/components/Followingbar';
-import PostContainer from '@/components/post/PostContainer';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/auth';
 import { redirect } from 'next/navigation';
+import { getDehydratedQuery, Hydrate } from '@/utils/react-query';
+import postQueryOptions from '@/service/post/server/queries';
+import userQueryOptions from '@/service/user/server/queries';
+import PostList from '@/components/post/PostList';
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
+
+  const postQuery = await getDehydratedQuery(postQueryOptions.all());
+  const userQuery = await getDehydratedQuery(userQueryOptions.all());
 
   if (!user) {
     redirect('/auth/signin');
@@ -16,8 +22,10 @@ export default async function HomePage() {
   return (
     <section className="flex w-full h-full justify-center mx-auto">
       <div className="w-full h-full max-w-[630px] sm:min-w-[630px] flex flex-col">
-        <Followingbar />
-        <PostContainer />
+        <Hydrate state={{ queries: [postQuery, userQuery] }}>
+          <Followingbar />
+          <PostList />
+        </Hydrate>
       </div>
       <UserSidebar user={user} />
     </section>
