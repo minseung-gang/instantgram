11ad9429bc05cd2bdd3a5e3f74309f221be738d4
minseung-gang <strause1@nate.com>
2024-10-17 23:28:48 +0900

    Fix: 유저 세션 데이터가 있을때 Navbar가 나오도록 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { ReactNode } from 'react';
 import AuthProvider from '@/context/AuthProvider';
 import ReactQueryProviders from '../components/commons/ReactQueryProvider';
 import Navbar from '@/components/commons/Navbar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './auth';
+import { redirect } from 'next/navigation';
 
 type Props = { children: ReactNode };
 
@@ -19,12 +22,13 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: Props) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="ko" className={`${openSans.className}`}>
       <body className="w-full flex overflow-auto">
         <AuthProvider>
           <ReactQueryProviders>
-            <Navbar />
+            {session && <Navbar />}
             <main className="w-full flex justify-center h-full overflow-scroll">
               {children}
             </main>
